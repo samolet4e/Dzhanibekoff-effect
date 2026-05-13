409ecb9a31db45812a2e0ad2e787ce3cf0707f98
Konstantin Metodiev <63013575+samolet4e@users.noreply.github.com>
2026-05-13 11:25:48 +0300

    Update f.m

--- private/f.m
+++ private/f.m
@@ -9,11 +9,13 @@ quat = x(4:7);
 % External torque
 M = [0.,0.,0.]';
 
+% Angular momentum conservation in body reference frame
 dw = invI*(M - cross(w,I*w));
 
-% Quaternion kinematics matrix
 p = w(1); q = w(2); r = w(3);
+% Skew symmetric matrix
 wx = [0,-r,q;r,0,-p;-q,p,0];
+% Quaternion kinematics matrix
 Omega = [0, -w'; w, -wx];
 
 dq = 0.5*Omega*quat;
